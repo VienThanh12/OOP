0fb5d86fbf09dc5971e7558ba732245c60c8371f
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-11-30 17:42:53 +0200

    Create Shape.cs

---
+++ OOP/Shapes/Shape.cs
@@ -0,0 +1,37 @@
+namespace Shapes
+{
+    public abstract class Shape : IComparable<Shape> // Example Shapes2
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Shape(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public abstract float CalculateArea();
+        // Example Shapes2
+        public int CompareTo(Shape other)
+        {
+            if (CalculateArea() < other.CalculateArea())
+            {
+                return -1;
+            }
+            else if (CalculateArea() > other.CalculateArea())
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
