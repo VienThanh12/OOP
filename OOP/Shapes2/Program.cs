f83f603c153e43a303cd2b0f7826fe1784f02176
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-11-30 17:40:41 +0200

    Update Program.cs

--- OOP/Shapes2/Program.cs
+++ OOP/Shapes2/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Welcome
+namespace Shapes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to spooky season!");
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Square(0, 0, 2));
+            shapes.Add(new Circle(1, 1, 2));
+            shapes.Add(new Square(-1, 3, 3));
+            shapes.Add(new Circle(-1, 5, 4));
+            shapes.Add(new Square(5, -2, 1));
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine(shape);
+            }
+
+            // Shapes2
+            Console.WriteLine("\n*** Sort ****\n");
+
+            shapes.Sort();
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine(shape);
+            }
+
+            // Shapes2 - background
+            /*
+            Console.WriteLine("\n*** Backround ****\n");
+            List<int> list = new List<int> { 1, 10, 6, 2, 7, 9 };
+            list.Sort();
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            */
         }
     }
 }
+
