2988f997d9d880c2b11a03255803105bae43acc4
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-11-30 17:51:10 +0200

    Update Program.cs

--- OOP/DiceThrow/Program.cs
+++ OOP/DiceThrow/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace Welcome
+namespace DiceObject2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to spooky season!");
+            // Example DiceThrows
+            // b)
+            Dice[] series = new Dice[5];
+            Dice dice;
+
+            for (int i = 0; i < series.Length; i++)
+            {
+                dice = new Dice(8);
+                dice.Throw();
+                series[i] = dice;
+            }
+
+            foreach (Dice d in series)
+            {
+                // Console.WriteLine(d);
+                Console.WriteLine(d.Result);
+            }
+
+            // a)
+            /*
+            Dice dice3 = new Dice(8);
+            int[] series = new int[5];
+
+            for (int i = 0; i < series.Length; i++)
+            {
+                dice3.Throw();
+                series[i] = dice3.Result;
+            }
+
+            foreach (int result in series)
+            {
+                Console.WriteLine(result);
+            }
+            */
+
+            // Example DiceObject2
+            /*
+            Dice dice1 = new Dice();
+            dice1.Throw();
+            Console.WriteLine(dice1.Faces);
+            Console.WriteLine(dice1.Result);
+            Console.WriteLine(dice1);
+
+            Dice dice2 = new Dice(20);
+            dice2.Throw();
+            Console.WriteLine(dice2);
+            */
         }
     }
 }
