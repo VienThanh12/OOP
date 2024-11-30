c28171e0e9aef5a926728f30209b99d755c2ef0a
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-11-30 17:50:12 +0200

    Create Dice.cs

---
+++ OOP/LoadedDice/Dice.cs
@@ -0,0 +1,59 @@
+namespace LoadedDice
+{
+    public class Dice
+    {
+        // attributes == class variables
+        private int _faces;
+
+        // manually created Property
+        public int Faces
+        {
+            get { return _faces; }
+            private set
+            {
+                if (value >= 1)
+                {
+                    _faces = value;
+                }
+                else
+                {
+                    _faces = 6;
+                }
+            }
+        }
+
+        // auto-implemented properties
+        public int Result { get; protected set; }
+        public Random Rnd { get; private set; }
+
+        // methods
+
+        // constructors
+        public Dice()
+        {
+            Faces = 6;
+            Result = 0;
+            Rnd = new Random();
+        }
+
+        public Dice(int faces)
+        {
+            Faces = faces;
+            Result = 0;
+            Rnd = new Random();
+        }
+
+        public virtual void Throw()
+        {
+            Result = Rnd.Next(1, Faces + 1);
+        }
+
+        // ToString method for getting the string
+        // representation of the class, for example
+        // for printing
+        public override string ToString()
+        {
+            return $"Dice: d{Faces}, result: {Result}";
+        }
+    }
+}
