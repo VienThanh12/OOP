03056e22748c0cb4c026d21a202a0db167c96b49
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-11-30 17:51:40 +0200

    Update Program.cs

--- OOP/DiceObjects2/Program.cs
+++ OOP/DiceObjects2/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Welcome
+namespace DiceObject2
 {
-    internal class Program
+    public class Dice
     {
-        static void Main(string[] args)
+        // attributes == class variables
+        private int _faces;
+
+        // manually created Property
+        public int Faces
         {
-            Console.WriteLine("Welcome to spooky season!");
+            get { return _faces; }
+            private set
+            {
+                if (value >= 1)
+                {
+                    _faces = value;
+                } else
+                {
+                    _faces = 6;
+                }
+            }
+        }
+
+        // auto-implemented properties
+        public int Result { get; protected set; }
+        public Random Rnd { get; private set; }
+
+        // methods
+
+        // constructors
+        public Dice()
+        {
+            Faces = 6;
+            Result = 0;
+            Rnd = new Random();
+        }
+
+        public Dice(int faces)
+        {
+            Faces = faces;
+            Result = 0;
+            Rnd = new Random();
+        }
+
+        public virtual void Throw()
+        {
+            Result = Rnd.Next(1, Faces + 1);
+        }
+
+        // ToString method for getting the string
+        // representation of the class, for example
+        // for printing
+        public override string ToString()
+        {
+            return $"Dice: d{Faces}, result: {Result}";
         }
     }
 }
