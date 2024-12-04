7d1f9279a41c8ff7f56b10b46489605f38633e90
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-12-04 16:19:40 +0200

    Activity 1

---
+++ OOP/Part1/Part1/Candidate.cs
@@ -0,0 +1,58 @@
+﻿namespace part1
+{
+    public class Candidate
+    {
+        // Properties with default get and set methods: auto-implemented properties
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string Party
+        {
+            get;
+            set;
+        }
+
+        private int _votes;
+        public int Votes
+        {
+            // make sure that the votes are not negative
+            get
+            {
+                return _votes;
+            }
+            set
+            {
+               if(value >= 0)
+                {
+                    _votes = value;
+                }
+                else
+                {
+                    _votes = 0;
+                }
+            }
+        }
+        // Methods
+        // Constructor
+        public Candidate(string first, string last, string party, int votes) 
+        {
+            FirstName = first;
+            LastName = last;
+            Party = party;
+
+            Votes = votes;
+        }
+        // Override ToString method 
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} ({Party}): {Votes}";
+        }
+    }
+}
---
+++ OOP/Part1/Part1/Program1.cs
@@ -0,0 +1,33 @@
+﻿/*
+The goal is to make sure you know basics of implementing and using a class and using a class, creating and using OO classes
+that have ralationships between them and that you know how to implement an interface
+ */
+namespace part1
+{
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            // PART 1 A
+            Console.WriteLine("Activity 1a");
+            Candidate candi = new Candidate("Jane", "Douglas", "Oxbox", 12724);
+            Console.WriteLine(candi);
+
+            // PART 1 B
+
+            Console.WriteLine("Activity 1b");
+
+            List<Candidate> candidates = new List<Candidate>();
+            candidates.Add(new Candidate("Jane", "Douglas", "Oxbox", 12724));
+            candidates.Add(new Candidate("Ellen", "Rose", "Oxtra", 10214));
+            candidates.Add(new Candidate("Luke", "Westaway", "Oxtra", 9011));
+            candidates.Add(new Candidate("Andy", "Farrant", "Oxbox", 13319));
+            candidates.Add(new Candidate("Mike", "Channel", "Oxbox", 8953));
+            candidates.Add(new Candidate("Johnny", "Chiodini", "Oxventure", 10466));
+            foreach (Candidate candidate in candidates)
+            {   
+                Console.WriteLine(candidate);
+            }
+        }
+    }
+}
